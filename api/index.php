2448c3ede1d8c9ea6a87fcba0bf344b932756942
ChoksiecutieXDD <roxas.t.bscs@gmail.com>
2026-07-04 16:11:19 +0800

    Trigger Vercel deployment on new repository URL

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
 
-// Forward Vercel requests to Laravel's public/index.php
+// Forward Vercel requests to Laravel's public/index.php - trigger build
 require __DIR__ . '/../public/index.php';
