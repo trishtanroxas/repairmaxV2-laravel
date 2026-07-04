38acf0844cf08e9543f53f6f45f51fee90170290
ChoksiecutieXDD <roxas.t.bscs@gmail.com>
2026-07-04 16:36:59 +0800

    Trigger Vercel build after changing Framework Preset to Other

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
 
-// Forward Vercel requests to Laravel's public/index.php
+// Forward Vercel requests to Laravel's public/index.php - trigger other preset
 require __DIR__ . '/../public/index.php';
+
 
